937e79d0f5476e69049ae2d34faa37a18503cbaf
Guilherme Burle Medeiros <143973903+Guilherme-burle@users.noreply.github.com>
2024-11-27 22:16:01 -0300

    Commit 3: Adiciona estilo com CSS e JavaScript

---
+++ Scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById('changeColorButton').addEventListener('click', () => {
+    document.body.style.backgroundColor =
+      document.body.style.backgroundColor === 'white' ? '#f5f5f5' : 'white';
+  });
+  
